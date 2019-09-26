81d9d3c7fea02774f4449a4c467b6b48f33859a9
STEPLADD3R <hello@stuartgreen.me.uk>
2019-09-26 17:33:38 +0100

    set up django-sass using scss 7-1 pattern

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party Apps
+    'django_sass',
     # Custom Apps
     'pages.apps.PagesConfig',
 ]
