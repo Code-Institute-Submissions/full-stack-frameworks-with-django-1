9bb91ed23fbaad8723bfc65979112692de129585
STEPLADD3R <hello@stuartgreen.me.uk>
2019-12-08 17:45:51 +0000

    added new policy pages and unified styling of user templates

--- pages/views.py
+++ pages/views.py
@@ -22,3 +22,45 @@ def home_view(request):
         'bugs': bugs,
     }
     return render(request, 'pages/home_view.html', context)
+
+
+def terms_and_conditions_view(request):
+    """
+        View for the refund policy as is required
+        on an e-commerce store
+    """
+    meta = {
+        'title': 'Terms & Conditions',
+    }
+    context = {
+        'meta': meta,
+    }
+    return render(request, 'pages/terms_view.html', context)
+
+
+def refund_policy_view(request):
+    """
+        View for the T&C's as is required
+        on an e-commerce store
+    """
+    meta = {
+        'title': 'Refund Policy',
+    }
+    context = {
+        'meta': meta,
+    }
+    return render(request, 'pages/refund_view.html', context)
+
+
+def privacy_policy_view(request):
+    """
+        View for the privacy policy as is required
+        on an e-commerce store
+    """
+    meta = {
+        'title': 'Privacy Policy',
+    }
+    context = {
+        'meta': meta,
+    }
+    return render(request, 'pages/privacy_view.html', context)
