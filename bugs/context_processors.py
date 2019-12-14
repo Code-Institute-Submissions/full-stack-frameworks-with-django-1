e008d83982566d3a75adc91634077c802cad01d4
STEPLADD3R <hello@stuartgreen.me.uk>
2019-12-14 00:22:05 +0000

    added bug counters to the menu and set up responsive columns for layout on bug-detail

--- bugs/context_processors.py
+++ bugs/context_processors.py
@@ -1,4 +1,4 @@
-from .models import Bug
+from .models import Bug, SavedBug
 
 
 def total_bug_counter(request):
@@ -11,11 +11,32 @@ def total_bug_counter(request):
     }
 
 
-# def user_submitted_bug_counter(request):
-#     """
-#         Helper Function to count
-#         only the current users submissions
-#     """
-#     return {
-#         'user_bug_count': Bug.objects.filter(author_id=request.user, status='IP').count(),
-#     }
+def user_submitted_bug_counter(request):
+    """
+        Helper Function to count
+        only the current users submissions
+    """
+    if request.user.is_authenticated:
+        return {
+            'user_bug_count': Bug.objects.filter(author=request.user, status='IP').count(),
+        }
+    else:
+        return {
+            'user_bug_count': 0,
+        }
+
+
+def user_saved_bug_counter(request):
+    """
+        Helper Function to count
+        only the current users saved
+        bugs
+    """
+    if request.user.is_authenticated:
+        return {
+            'user_saved_bug_count': SavedBug.objects.filter(user=request.user).count(),
+        }
+    else:
+        return {
+            'user_saved_bug_count': 0,
+        }
